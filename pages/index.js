c10e0cb3989c4a09d52fad672401229cb1875c2c
dovanthao2404 <dovanthao2404@gmail.com>
2022-03-16 14:26:32 +0700

    fix header

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import { useEffect } from 'react/cjs/react.development';
+import { useEffect } from 'react';
 export default function Home() {
   const router = useRouter();
 
